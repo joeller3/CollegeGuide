3c1f5bad395576b278cd6d09ba2538f2f6bad34a
TEST <test@TESTs-MacBook-Pro.local>
2017-02-23 16:37:37 -0500

    took out debugging comments

--- db.php
+++ db.php
@@ -1,34 +1,5 @@
 
      <?
-     
-     /*
- *      SELECT college_id
-						FROM colleges
-						WHERE name = 'Brandeis University'
-                        OR
-                        name = 'Amherst College'
-                        OR 
-                        name = 'Elon University';
-                        
-SELECT firstName, lastName, email, name
-FROM alumni, colleges
-WHERE alumni.college_id = colleges.college_id;
-
-SELECT firstName, lastName, email, name
-FROM alumni, colleges
-WHERE alumni.college_id = colleges.college_id
-AND colleges.college_id = 2;
-       
-*/
-    //PHP7 DB functions:
-    //mysqli_fetch_array() - Fetch a result row as an associative, a numeric array, or both
-    //mysqli_fetch_row() - Get a result row as an enumerated array
-    //mysqli_fetch_object() - Returns the current row of a result set as an object
-    //mysqli_query() - Performs a query on the database
-    //mysqli_data_seek() - Adjusts the result pointer to an arbitrary row in the result
-     
-        //phpinfo();
-        
         //debugging 
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
@@ -62,8 +33,5 @@ AND colleges.college_id = 2;
         function query($query){
             global $link;
             return mysqli_query($link, $query); 
-        }
-        
-        //$result->free();
-        //$link->close();       
+        }   
     ?>
